860bfe5ad4c2bf62658732309ed67c4739c2448e
Chris Ballinger <chrisballinger@gmail.com>
2013-05-25 13:51:13 -0700

    Use South

--- push/push/settings.py
+++ push/push/settings.py
@@ -123,9 +123,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    # 3rd Party
+    'south',
+
 )
 
 # A sample logging configuration. The only tangible logging
