af198032ed250abbc70bcbacd406063444acd34b
David Brodsky <davidpbrodsky@gmail.com>
2015-06-23 10:41:34 -0700

    TokenSerializer : limit device queryset to user-owned

--- push/tokens/serializers.py
+++ push/tokens/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
+from push_notifications.models import APNSDevice, GCMDevice
 from tokens.models import Token
 
 
 class TokenSerializer(serializers.HyperlinkedModelSerializer):
+
+    apns_device = serializers.PrimaryKeyRelatedField(required=False, queryset=APNSDevice.objects.all())
+    gcm_device = serializers.PrimaryKeyRelatedField(required=False, queryset=GCMDevice.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        super(TokenSerializer, self).__init__(*args, **kwargs)
+        if self.context.get('request', None):
+            # Restrict the queryset of APNS and GCM devices to those owned by the request's user
+            apns_device = self.fields.get('apns_device')
+            apns_device.queryset = apns_device.queryset.filter(user=self.context['request'].user)
+
+            gcm_device = self.fields.get('gcm_device')
+            gcm_device.queryset = gcm_device.queryset.filter(user=self.context['request'].user)
+
     class Meta:
         model = Token
-        fields = ('id', 'url', 'name', 'token',)
+        fields = ('id', 'url', 'name', 'token', 'apns_device', 'gcm_device')
         read_only_fields = ('token',)
