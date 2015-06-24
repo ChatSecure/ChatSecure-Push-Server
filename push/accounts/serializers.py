5932bc072974dc3191234829786fc1ab56334b2d
David Brodsky <davidpbrodsky@gmail.com>
2015-06-24 16:21:22 -0700

    remove pks from API
    use RegistrationIdRelatedField to relate GCM / APNS Devices to Tokens

--- push/accounts/serializers.py
+++ push/accounts/serializers.py
@@ -3,7 +3,6 @@ from accounts.models import PushUser
 
 
 class CreateUserSerializer(serializers.Serializer):
-    client_id = serializers.CharField(max_length=100)
     email = serializers.EmailField(max_length=100, required=False)
     username = serializers.CharField(max_length=30)
     password = serializers.CharField(max_length=100)
@@ -12,5 +11,5 @@ class CreateUserSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = PushUser
-        fields = ('id', 'username', 'email')
+        fields = ('username', 'email')
         read_only_fields = ('username',)
