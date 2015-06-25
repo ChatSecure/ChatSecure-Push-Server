d22b9ab5026b13ac085ca708c33c4fca895e3c1f
David Brodsky <davidpbrodsky@gmail.com>
2015-06-25 11:24:06 -0700

    attempt to pull APNS cert from config var to file in post_compile

--- push/push/settings.py
+++ push/push/settings.py
@@ -189,6 +189,12 @@ import dj_database_url
 
 DATABASES['default'] = dj_database_url.config()
 
+APNS_CERT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'private_keys/apns_cert.pem' )
+PUSH_NOTIFICATIONS_SETTINGS = {
+    'APNS_CERTIFICATE' : APNS_CERT,
+    'GCM_API_KEY' : os.environ.get('GCM_API_KEY', '')
+}
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
