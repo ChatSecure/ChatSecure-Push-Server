e9daf3b998357edcff5f6ac6c68eddd389fa1a54
David Brodsky <davidpbrodsky@gmail.com>
2015-06-24 18:45:04 -0700

    grab DJANGO_SECRET_KEY from environment if available

--- push/push/settings.py
+++ push/push/settings.py
@@ -90,7 +90,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
