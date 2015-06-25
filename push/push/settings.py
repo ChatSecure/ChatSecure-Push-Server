95cc758ac206c12e189d0a56d22fb400313b042b
David Brodsky <davidpbrodsky@gmail.com>
2015-06-25 12:11:13 -0700

    Heroku friendly Logging config

--- push/push/settings.py
+++ push/push/settings.py
@@ -154,32 +154,19 @@ CHATSECURE_PUSH = {
     'USE_MESSAGE_QUEUE': True
 }
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
         },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+        }
     }
 }
 
