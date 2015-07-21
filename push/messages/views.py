4045bab224349c239d45cac3b0cd0e0866a2afe9
David Brodsky <davidpbrodsky@gmail.com>
2015-07-21 12:03:43 -0700

    Send actual push payload

--- push/messages/views.py
+++ push/messages/views.py
@@ -31,17 +31,44 @@ class MessagesViewSet(viewsets.ViewSet):
 
 
 def send_message(token=None, data=None, broadcast=True):
+    '''
+
+    Send a push message containing to the owner of the provided token.
+    data is packaged within a 'message' dictionary as below:
+
+    { ... APNS / GCM Payload
+        'message' : {
+            'token' : 'recipient_whitelist_token',
+            'data' : 'data'
+        }
+    }
+
+    See "APNS Documentation":https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html
+    See "GCM Documentation":https://developers.google.com/cloud-messaging/server-ref#downstream
+
+    :param token: the whitelist token corresponding to the specific device to push, if broadcast is False
+    :param data: APNS currently supports 2kb and GCM 4kb
+    :param broadcast: whether to alert all devices belonging to recipient (default True)
+    '''
+
+    message = {
+        'message': {
+            'data': data,
+            'token': token.token
+        }
+    }
+
     recipient = token.owner
     if broadcast is True:
         apns_devices = APNSDevice.objects.filter(user__pk=recipient.pk)
         gcm_devices = GCMDevice.objects.filter(user__pk=recipient.pk)
-        apns_devices.send_message("Hello Broadcast")
-        gcm_devices.send_message("Hello Broadcast")
+        apns_devices.send_message(message=None, extra=message, content_available=True)
+        gcm_devices.send_message(message)
     else:
         apns_device = token.apns_device
         gcm_device = token.gcm_device
 
         if apns_device:
-            apns_device.send_message("Hello")
+            apns_device.send_message(message=None, extra=message, content_available=True)
         elif gcm_device:
-            gcm_device.send_message("Hello")
+            gcm_device.send_message(message)
