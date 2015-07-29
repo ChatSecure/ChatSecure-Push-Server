fa2ac8d998e84688d378096e970875cd2a5e2bfe
David Brodsky <davidpbrodsky@gmail.com>
2015-07-29 08:57:08 -0700

    device viewset docstring swap
    had gcm and apns docstrings mixed up

--- push/devices/views.py
+++ push/devices/views.py
@@ -14,18 +14,15 @@ class DeviceViewSetMixin(object):
 
 class APNSDeviceAuthorizedViewSet(OwnerOnlyPermission, DeviceViewSetMixin, ModelViewSet):
     """
-    A [GCM](https://developers.google.com/cloud-messaging/server-ref#downstream) compatible device.
+    An [APNS](https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html) compatible device.
 
     ## Registration ID (Required)
 
-    Your registration ID is the [`GoogleCloudMessaging.INSTANCE_ID_SCOPE`](https://github.com/googlesamples/google-services/blob/e06754fc7d0e4bf856c001a82fb630abd1b9492a/android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/RegistrationIntentService.java#L54) token.
-
-    When this identifier changes you should use this API to update your device.
+    The APNS Device Token, presented as a 64 character string.
 
     ## Device ID (Optional)
 
-    Currently unused. Any unique identifier you'd like to use to identify this device as its Registraion ID changes.
-    Android does not provide a universal device identifier, but there are [reasonable solutions](http://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id/2853253#2853253)
+    Currently unused. Any unique identifier you'd like to use to identify this device.
 
     ## Next Steps
 
@@ -42,15 +39,18 @@ class APNSDeviceAuthorizedViewSet(OwnerOnlyPermission, DeviceViewSetMixin, Model
 
 class GCMDeviceAuthorizedViewSet(OwnerOnlyPermission, DeviceViewSetMixin, ModelViewSet):
     """
-    An [APNS](https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html) compatible device.
+    A [GCM](https://developers.google.com/cloud-messaging/server-ref#downstream) compatible device.
 
     ## Registration ID (Required)
 
-    The APNS Device Token, presented as a 64 character string.
+    Your registration ID is the [`GoogleCloudMessaging.INSTANCE_ID_SCOPE`](https://github.com/googlesamples/google-services/blob/e06754fc7d0e4bf856c001a82fb630abd1b9492a/android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/RegistrationIntentService.java#L54) token.
+
+    When this identifier changes you should use this API to update your device.
 
     ## Device ID (Optional)
 
-    Currently unused. Any unique identifier you'd like to use to identify this device.
+    Currently unused. Any unique identifier you'd like to use to identify this device as its Registraion ID changes.
+    Android does not provide a universal device identifier, but there are [reasonable solutions](http://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id/2853253#2853253)
 
     ## Next Steps
 
