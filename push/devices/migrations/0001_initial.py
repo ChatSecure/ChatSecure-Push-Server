107b6e06a4782905377b911ed929d8e179c33c15
David Chiles <dwalterc@gmail.com>
2015-08-11 14:57:11 -0700

    Added missing migrations for devices & accounts

---
+++ push/devices/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import uuid
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='APNSDevice',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('name', models.CharField(max_length=255, null=True, verbose_name='Name', blank=True)),
+                ('active', models.BooleanField(default=True, help_text='Inactive devices will not be sent notifications', verbose_name='Is active')),
+                ('date_created', models.DateTimeField(auto_now_add=True, verbose_name='Creation date', null=True)),
+                ('registration_id', models.TextField(verbose_name='Registration ID')),
+                ('device_id', models.TextField(null=True, verbose_name='Device ID', blank=True)),
+                ('owner', models.ForeignKey(related_name='apns_devices', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'APNS device',
+            },
+        ),
+        migrations.CreateModel(
+            name='GCMDevice',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('name', models.CharField(max_length=255, null=True, verbose_name='Name', blank=True)),
+                ('active', models.BooleanField(default=True, help_text='Inactive devices will not be sent notifications', verbose_name='Is active')),
+                ('date_created', models.DateTimeField(auto_now_add=True, verbose_name='Creation date', null=True)),
+                ('registration_id', models.TextField(verbose_name='Registration ID')),
+                ('device_id', models.TextField(null=True, verbose_name='Device ID', blank=True)),
+                ('owner', models.ForeignKey(related_name='gcm_devices', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'GCM device',
+            },
+        ),
+    ]
---
+++ push/tokens/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('devices', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Token',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100, null=True, blank=True)),
+                ('token', models.CharField(unique=True, max_length=100, verbose_name=b'Whitelist Token')),
+                ('apns_device', models.ForeignKey(blank=True, to='devices.APNSDevice', null=True)),
+                ('gcm_device', models.ForeignKey(blank=True, to='devices.GCMDevice', null=True)),
+                ('owner', models.ForeignKey(related_name='tokens', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
