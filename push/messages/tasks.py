5af7113b0c071ac01a6dedbed473f559d07c58f3
David Brodsky <davidpbrodsky@gmail.com>
2016-02-08 17:41:29 -0800

    sharedTask -> task

--- push/messages/tasks.py
+++ push/messages/tasks.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
 
-from celery import shared_task
+from celery import task
 from messages.messenger import send_apns, send_gcm
 
-@shared_task
+
+@task(ignore_result=True)
 def task_send_apns(registration_ids, message, **kwargs):
     return send_apns(registration_ids, message, **kwargs)
 
-@shared_task
+
+@task(ignore_result=True)
 def task_send_gcm(registration_ids, message, **kwargs):
     return send_gcm(registration_ids, message, **kwargs)
