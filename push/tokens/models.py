24b3ada014edae01d8fd0391d8b2b79f1e7b5bdd
David Brodsky <davidpbrodsky@gmail.com>
2016-02-18 18:52:06 -0800

    models: Add token date_created
    also fix token.token verbose_name

--- push/tokens/models.py
+++ push/tokens/models.py
@@ -1,11 +1,15 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext as _
+
 from devices.models import APNSDevice, GCMDevice
 
 
 class Token(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
-    token = models.CharField('Whitelist Token', max_length=100, unique=True)
+    token = models.CharField(verbose_name=_('Whitelist Token'), max_length=100, unique=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tokens')
     apns_device = models.ForeignKey(APNSDevice, blank=True, null=True)
     gcm_device = models.ForeignKey(GCMDevice, blank=True, null=True)
+    # null=True so tokens created before migration can exist
+    date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
