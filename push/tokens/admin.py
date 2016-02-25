eaa3e93d3fb2bef41eb010d47127b1c3214dae75
David Brodsky <davidpbrodsky@gmail.com>
2016-02-24 16:52:40 -0800

    update Token ModelAdmin

--- push/tokens/admin.py
+++ push/tokens/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from tokens.models import Token
 
-admin.site.register(Token)
+
+class TokenAdmin(admin.ModelAdmin):
+    # by default auto_now_add fields are not displayed. We want to display (but not edit) token creation date
+    readonly_fields = ('date_created',)
+
+admin.site.register(Token, TokenAdmin)
--- push/tokens/models.py
+++ push/tokens/models.py
@@ -13,3 +13,8 @@ class Token(models.Model):
     gcm_device = models.ForeignKey(GCMDevice, blank=True, null=True)
     # null=True so tokens created before migration can exist
     date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
+
+    def __unicode__(self):
+        owner_name = self.owner.username
+        token_type = "APNS" if self.apns_device is not None else "GCM"
+        return "%s's %s token created %s" % (owner_name, token_type, self.date_created)
