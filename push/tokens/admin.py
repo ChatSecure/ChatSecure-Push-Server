7223d4da1fee5bd858501897f9d5f01c795dc6c2
David Brodsky <davidpbrodsky@gmail.com>
2016-05-11 16:09:19 -0700

    Token Admin: Allow searching tokens by token string

--- push/tokens/admin.py
+++ push/tokens/admin.py
@@ -5,5 +5,6 @@ from tokens.models import Token
 class TokenAdmin(admin.ModelAdmin):
     # by default auto_now_add fields are not displayed. We want to display (but not edit) token creation date
     readonly_fields = ('date_created',)
+    search_fields = ['token',]
 
 admin.site.register(Token, TokenAdmin)
